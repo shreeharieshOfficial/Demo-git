6d4be83214c8c1ef524a437eaadacb68409e7514
shreeharieshOfficial <shreehariesh.m@pelorus.in>
2026-04-23 14:58:09 +0530

    new upated code

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 print("Hello World")
 
 
-def evidence(a, b):
+def evidence(a, b, num):
     c = a - b
+    print(num)
     return c
 
 
-print(evidence(20, 10))
+print(evidence(20, 10, 100))
 
 
 # lsjbdvnlksd
-def case(a, b):
+def case(a, b, value):
     c = a + b
+    print(value)
     return c
 
 
 # sljbdv
-print(case(1, 2))
+print(case(1, 2, 500))
 
 
 print("I called onga to say he is ponga..... Nachoooooooo")
+
+
